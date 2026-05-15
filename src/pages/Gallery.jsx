a2c71051bb9d78c3cf9edfa16371e7f576adf24e
nuhmanudheen t <nuhmanudheent@gmail.com>
2026-05-15 12:09:00 +0530

    udpate

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -11,9 +11,9 @@ const Gallery = () => {
         { id: 1, category: 'Campus', url: 'https://images.unsplash.com/photo-1591123120675-6f7f1aae0e5b?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2Nob29sJTIwY2FtcHVzfGVufDB8fDB8fHww' },
         { id: 2, category: 'Classroom', url: 'https://images.unsplash.com/photo-1509062522246-3755977927d7?q=80&w=800' },
         { id: 3, category: 'Events', url: 'https://images.unsplash.com/photo-1540575467063-178a50c2df87?q=80&w=800' },
-        { id: 4, category: 'Sports', url: 'https://images.unsplash.com/photo-1511871893393-82e9c1d39d0d?q=80&w=800' },
+        { id: 4, category: 'Sports', url: 'https://upload.wikimedia.org/wikipedia/commons/9/92/Youth-soccer-indiana.jpg' },
         { id: 5, category: 'Activities', url: 'https://images.unsplash.com/photo-1564069114553-7215e1ff1890?q=80&w=800' },
-        { id: 6, category: 'Campus', url: 'https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=800' },
+        { id: 6, category: 'Campus', url: 'https://media.istockphoto.com/id/1768985857/photo/university-students-hanging-out-in-campus.jpg?s=612x612&w=0&k=20&c=AcDwJLF-DzVdQTMz2rS7_TAAUYZnQNjPydcoufQK25s=' },
         { id: 7, category: 'Classroom', url: 'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=800' },
         { id: 8, category: 'Activities', url: 'https://images.unsplash.com/photo-1523580494863-6f3031224c94?q=80&w=800' },
     ];
